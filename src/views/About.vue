63764339a75079460fdf754364460e89515ff0cd
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-02-02 20:59:54 +0100

    :lipstick: fix about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="about">
-    <div class="logo">
-      <div class="memo">
-        Memo
-      </div>
-      <img class="fdc-logo" src="@/assets/fresque-full.png" />
-    </div>
+    <img class="fdc-logo" src="@/assets/fresque-full.png" />
     <p>
       Le memo de la fresque du climat a pour but de proposer aux animateurs un
       accés rapide aux cartes du jeu.
@@ -25,32 +20,13 @@ export default {
 };
 </script>
 <style scoped>
-.logo {
-  position: relative;
-  width: 90%;
-  margin: 0 auto 3rem auto;
-}
-.memo {
-  color: #04c2c0;
-  text-transform: uppercase;
-  /* font-family: 'Arial Black', Gadget, sans-serif; */
-  font-family: Courier, monospace;
-  font-weight: 700;
-  font-size: 3.5rem;
-  transform: rotate(-0.03turn) translate(-4.5rem, 1.5rem);
-  text-shadow: #e90000 1px 1px;
-  position: relative;
-  top: 0px;
-}
 .fdc-logo {
-  position: relative;
-  top: 0px;
+  width: 90vw;
 }
 .about {
   margin: 0 0.5rem;
-  color: grey;
 }
 p {
-  margin: 1rem 3rem;
+  margin: 1rem 1rem;
 }
 </style>
