0ed9e4fa610b7a766bc1d065d9f03d613a51a56a
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-02-03 13:11:46 +0100

    :recycle: cause and consequences list

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -1,34 +1,14 @@
 <template>
   <div class="card-details-panel">
     <img class="card-image" :src="card.img.url" :title="card.shortTitle" />
-    <h2 class="causes-title">
-      <div class="badge">
-        {{ card.causes.length }}
-      </div>
-      cause<span v-if="card.causes.length > 1">s</span>
-    </h2>
-    <div class="causes">
-      <Cause v-for="cause in card.causes" :key="cause.id" :cause="cause" />
-    </div>
-    <h2 class="consequences-title">
-      <div class="badge">
-        {{ card.consequences.length }}
-      </div>
-      consequence<span v-if="card.consequences.length > 1">s</span>
-    </h2>
-    <div class="consequences">
-      <Consequence
-        v-for="consequence in card.consequences"
-        :key="consequence.id"
-        :consequence="consequence"
-      />
-    </div>
+    <CauseList :causes="card.causes" />
+    <ConsequenceList :consequences="card.consequences" />
   </div>
 </template>
 
 <script>
-import Cause from '@/components/Cause.vue';
-import Consequence from '@/components/Consequence.vue';
+import CauseList from '@/components/CauseList.vue';
+import ConsequenceList from '@/components/ConsequenceList.vue';
 
 export default {
   name: 'CardView',
@@ -36,8 +16,8 @@ export default {
     card: Object
   },
   components: {
-    Cause,
-    Consequence
+    CauseList,
+    ConsequenceList
   }
 };
 </script>
@@ -53,36 +33,5 @@ export default {
   max-width: 600px;
   padding: 0;
   margin: 3px;
-}
-.badge {
-  color: #ffffff;
-  background-color: #04c2c0;
-  margin: 0 0.5rem;
-  width: 2rem;
-  height: 2rem;
-  border-radius: 1rem;
-  display: inline-block;
-  font-size: 1.6rem;
-  text-shadow: #e90000 1px 1px;
-  box-shadow: 1px 1px 0px #706f71;
-}
-.causes-title {
-  display: flex;
-  justify-content: flex-start;
-}
-.consequences-title {
-  display: flex;
-  justify-content: flex-end;
-  margin-right: 0.3rem;
-}
-.causes {
-  display: flex;
-  flex-direction: column;
-  max-width: 600px;
-}
-.consequences {
-  display: flex;
-  flex-direction: column;
-  max-width: 600px;
 }
 </style>
---
+++ src/components/ConsequenceList.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2 class="consequence-title">
+    <div class="badge">
+      {{ consequences.length }}
+    </div>
+    consequence<span v-if="consequences.length > 1">s</span>
+  </h2>
+  <div class="consequences">
+    <Consequence
+      v-for="consequence in consequences"
+      :key="consequence.id"
+      :consequence="consequence"
+    />
+  </div>
+</template>
+
+<script>
+import Consequence from './Consequence.vue';
+
+export default {
+  name: 'CauseList',
+  props: {
+    consequences: Object
+  },
+  components: {
+    Consequence
+  }
+};
+</script>
+
+<style scoped>
+.consequences-title {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 0.3rem;
+}
+
+.badge {
+  color: #ffffff;
+  background-color: #04c2c0;
+  margin: 0 0.5rem;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+  display: inline-block;
+  font-size: 1.6rem;
+  text-shadow: #e90000 1px 1px;
+  box-shadow: 1px 1px 0px #706f71;
+}
+.consequences {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+}
+</style>
