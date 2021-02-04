db0603d21878baba3637dbe0f61f556ae5a0709b
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-02-04 19:09:57 +0100

    :lipstick: :construction: temporary arrow svg

--- src/components/Cause.vue
+++ src/components/Cause.vue
@@ -15,7 +15,29 @@
     /></router-link>
     <div class="arrow-anchor">
       <span class="arrow" :class="getLinkStyle('arrow-', cause.link.status)">
-        &#10145;
+        <svg
+          version="1.1"
+          id="Capa_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="444.815px"
+          height="444.815px"
+          viewBox="0 0 444.815 444.815"
+          xml:space="preserve"
+        >
+          <g>
+            <path
+              d="M421.976,196.712L236.111,10.848C228.884,3.615,220.219,0,210.131,0c-9.9,0-18.464,3.615-25.697,10.848L163.023,32.26
+			c-7.234,6.853-10.85,15.418-10.85,25.697c0,10.277,3.616,18.842,10.85,25.697l83.653,83.937H45.677
+			c-9.895,0-17.937,3.568-24.123,10.707s-9.279,15.752-9.279,25.837v36.546c0,10.088,3.094,18.698,9.279,25.837
+			s14.228,10.704,24.123,10.704h200.995L163.02,360.88c-7.234,7.228-10.85,15.89-10.85,25.981c0,10.089,3.616,18.75,10.85,25.978
+			l21.411,21.412c7.426,7.043,15.99,10.564,25.697,10.564c9.899,0,18.562-3.521,25.981-10.564l185.864-185.864
+			c7.043-7.043,10.567-15.701,10.567-25.981C432.54,211.939,429.016,203.37,421.976,196.712z"
+            />
+          </g>
+        </svg>
       </span>
     </div>
     <p class="cause-explanation" v-if="!!cause.link.Explanation">
@@ -96,28 +118,22 @@ export default {
 }
 .arrow-anchor {
   position: relative;
+  width: 5px;
+  height: 5px;
   float: left;
 }
-.arrow {
-  margin: 0;
-  padding: 0;
-  float: left;
-  color: #706f71;
-
-  font-size: calc(3.6rem + 0vw);
-  position: absolute;
-  top: -1rem;
-  left: -2.9rem;
+.arrow svg {
   z-index: -1;
-  transform: scaleX(0.7);
+  transform: translate(-50%, -50%) scale(0.08) translate(-50%, 50%);
+  fill: #706f71;
 }
-.arrow-valid {
-  color: #04c2c0;
+.arrow-valid svg {
+  fill: #04c2c0;
 }
-.arrow-optional {
-  color: rgb(255, 221, 103);
+.arrow-optional svg {
+  fill: rgb(255, 221, 103);
 }
-.arrow-invalid {
-  color: #e90000;
+.arrow-invalid svg {
+  fill: #e90000;
 }
 </style>
