30cc30f3e28e272c848456f75b63dfe607c615ef
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-04-13 10:49:02 +0200

    banner2 no selection

--- src/components/collage/CollageBanner2.vue
+++ src/components/collage/CollageBanner2.vue
@@ -1,6 +1,8 @@
 <template>
-  <div v-if="selectedCard" class="banner">
-    <span>{{ selectedCard.title }}</span>
+  <div class="bg">
+    <div v-if="selectedCard" class="banner">
+      <span>{{ selectedCard.title }}</span>
+    </div>
   </div>
 </template>
 
@@ -15,13 +17,15 @@ export default {
 </script>
 
 <style scoped>
+.bg {
+  background-color: #efeeee;
+  height: 67px;
+}
 .banner {
   padding: 1rem;
   font-size: 1.8rem;
   font-weight: 600;
   color: #04c2c0;
   text-shadow: #5a5a5a 1px 1px;
-
-  background-color: #efeeee;
 }
 </style>
