1935784e5a54f01756cd708c76f5c734643c9aca
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-04-13 05:56:48 +0200

    collage/:layout route

--- src/services/LayoutService.js
+++ src/services/LayoutService.js
@@ -2,14 +2,27 @@ import layoutSet1 from '@/data/layouts/set1.js';
 import layoutSet2 from '@/data/layouts/set2.js';
 
 export default {
+  validLayouts() {
+    return ['set1', 'set2'];
+  },
+  isValidLayout(layoutName) {
+    return layoutName === '' || this.validLayouts().includes(layoutName);
+  },
+  defaultLayout() {
+    return layoutSet2;
+  },
   getLayoutByName(layoutName) {
     switch (layoutName) {
+      case '':
+        return this.defaultLayout();
       case 'set1':
         return layoutSet1;
       case 'set2':
         return layoutSet2;
       default:
-        throw new Error(`The layout name '${layoutName}' is not supported`);
+        console.warn(`The layout name '${layoutName}' is not supported`);
+        return this.defaultLayout();
+      // throw new Error(`The layout name '${layoutName}' is not supported`);
     }
   },
 };
