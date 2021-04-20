971fe48113caf9e4dd0f4727f53fd383e3145a33
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-04-20 10:53:09 +0200

    toggle cards view list/menu

---
+++ src/components/collections/GridIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="35px"
+    height="35px"
+    viewBox="0 0 35 35"
+    style="enable-background: new 0 0 35 35"
+    xml:space="preserve"
+    :style="SvgStyle"
+  >
+    <g>
+      <rect width="16" height="16" rx="4" ry="4" />
+      <rect x="19" width="16" height="16" rx="4" ry="4" />
+      <rect y="19" width="16" height="16" rx="4" ry="4" />
+      <rect x="19" y="19" width="16" height="16" rx="4" ry="4" />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    color: String,
+  },
+  computed: {
+    SvgStyle() {
+      return { fill: this.color };
+    },
+  },
+};
+</script>
---
+++ src/components/collections/ToggleView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="toggle">
+    <grid-icon
+      class="icon"
+      alt="small cards"
+      title="small cards"
+      :color="colorSmall"
+      @click="handleClick(true)"
+    />
+    <lines-icon
+      class="icon"
+      alt="big cards"
+      title="big cards"
+      :color="colorBig"
+      @click="handleClick(false)"
+    />
+  </div>
+</template>
+
+<script>
+import GridIcon from './GridIcon.vue';
+import LinesIcon from './LinesIcon.vue';
+export default {
+  components: { GridIcon, LinesIcon },
+  props: {
+    smallIconSelected: Boolean,
+  },
+  computed: {
+    colorSmall() {
+      // console.log('this.smallIconSelected', this.smallIconSelected);
+      return this.smallIconSelected ? '#04c2c0' : '#706f71';
+    },
+    colorBig() {
+      return this.smallIconSelected ? '#706f71' : '#04c2c0';
+    },
+  },
+  methods: {
+    handleClick(selection) {
+      console.log('handleClick', selection);
+      this.$emit('selectionChange', selection);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon {
+  min-width: 25px;
+  min-height: 25px;
+  width: min(3vw, 35px);
+  height: min(3vw, 35px);
+  padding: 5px;
+  border-radius: 5px;
+}
+.toggle .icon {
+  border: 2px solid #fff;
+}
+.toggle .icon:hover {
+  border: 2px solid #04c2c0;
+}
+</style>
