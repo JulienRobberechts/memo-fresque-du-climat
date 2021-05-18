e8438348727dc353a5a8316726e4db5c9f05b608
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-05-18 10:23:33 +0200

    CardExplanation

---
+++ src/components/cards/CardExplanation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="explanation" v-if="card.explanation">
+    <span class="explanation-logo">
+      <img src="@/assets/fresque-hand.png" />
+    </span>
+    {{ card.explanation }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardExplanation',
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style scoped>
+.explanation-logo img {
+  position: relative;
+  width: 1.5rem;
+}
+.explanation {
+  margin: 1rem 1rem;
+  max-width: 600px;
+  font-size: 1.2rem;
+  text-align: justify;
+}
+</style>
